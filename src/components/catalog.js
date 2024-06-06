052c44b4a910ab40a6c019a2657f154f17010766
dasha ryabina <ryabinadasha@gmail.com>
2024-06-06 21:06:42 +0300

    new catalog

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -50,12 +50,14 @@ const Catalog = () => {
             }
             console.log(err.config);
         })
+    }
+    
+    useEffect(() => {
         if (message === "" && problems.length < 1 && !fetching) {
             setMessage("No problems added yet");
         }
-    }
-    
-    
+    }, [problems])
+
     useEffect(() => {
         if (localStorage.getItem("access")) {
             setLoggedIn(true);
@@ -66,90 +68,117 @@ const Catalog = () => {
     }, [])
 
     return (
-<body className="flex-column">
-    <main className="catalog main">
-        <section className="overviewSection">
-        <div className="headerContainer">
-                <div className="navContainer">
-                    <div className="brandContainer">
-                        <img className="brandLogo" src={logo} alt="alt text" />
-                        <div className="navMenu">     
-                        <Link to="/"><h2 className="homeLink">Главная</h2></Link>
-                        <Link to="/profile"><h2 className="profileLink">Личный кабинет</h2></Link>
-                        <Link to="/catalog"><h2 className="problemsCatalogLink">Каталог проблем</h2></Link>
-                        </div>
+        <body class="flex-column">
+    <main class="catalog-page main">
+        <section class="mainSection">
+            <div class="flexRow">
+                <img class="logoImage" src={logo} alt="alt text" />
+                <div class="headerRow">
+                    <div class="navigationRow">
+                        <Link to="/"><h2 className="homeTitle">Главная</h2></Link> 
+                        <Link to="/profile"><h2 className="personalCabinetTitle">Личный кабинет</h2></Link>
+                        <Link to="/catalog"><h2 className="catalogTitle">Каталог проблем</h2></Link>                  
+                        
                     </div>
-                    <div className="authContainer">
-            {loggedIn ? (
-                <button onClick={signOut}><h5 className="loginLink">Выйти</h5></button>
-            ) : (
+                    <div class="authBox">
+                    {loggedIn ? (
+                        <button onClick={signOut}><h5 className="loginTitle">Выйти</h5></button>
+                        ) : (
                 <>
-                    <Link to="/login"><h2 className="loginLink">Войти</h2></Link>
-                    <Link to="/register"><h2 className="registerLink">Регистрация</h2></Link>
+                    <Link to="/login"><h2 className="loginTitle">Войти</h2></Link>
+                    <Link to="/register"><h2 className="registerTitle">Регистрация</h2></Link>
                 </>
-            )}
-        </div>
-                </div>
-                <div className="contentBoxCatalog">
-                    <h2 className="catalogTitle">Каталог проблем</h2>
+            )} 
+                    </div>
                 </div>
             </div>
-            <div class="mainContentRow">
-                    <div class="commonDiseasesColumn">
-                        <h4 class="highlight1">Наиболее распространённые заболевания психики:</h4>
-                        <div class="diseasesListColumn">
-                            <h5 class="commonIssuesBox_box">
-                                <span class="commonIssuesBox"
-                    ><span class="commonIssuesBox_span0"><br /></span><span class="commonIssuesBox_span1">Невротические расстройства</span><span class="commonIssuesBox_span2">
-                      (легкая психопатология):<br />Тревожные      расстройства:<br />Генерализованное       тревожное
-                      расстройство<br />Паническое       расстройство<br />Социофобия<br />Расстройство
-                            обсессивно-компульсивного спектра<br />Посттравматическое       стрессовое расстройство<br />Различные
-                      фобии<br />Аффективные      расстройства:<br />Депрессия<br />Биполярное аффективное
-                      расстройство<br />Расстройства, связанные с соматическими симптомами:<br />Соматоформные
-                            расстройства<br />Нарушения сна:<br />Бессонница<br />Гиперсомния</span></span>
-                            </h5>
-                            <h5 class="severeIssuesBox_box">
-                                <span class="severeIssuesBox"
-                    ><span class="severeIssuesBox_span0"
-                      >Психопатические расстройства (тяжелая психопатология):<br /></span><span class="severeIssuesBox_span1">Шизофрения<br />Бредовые расстройства<br /></span><span class="severeIssuesBox_span2">Личностные (персонификационные) расстройства:<br /></span><span class="severeIssuesBox_span3"
-                      >Параноидальное      расстройство личности<br />Шизоидное расстройство личности<br />Нарциссическое
-                           расстройство личности<br />Граничное расстройство личности (эмоционально нестабильное)<br />Избегающее
-                      (тревожное) расстройство личности<br />Зависимое расстройство личности<br />Антисоциальное
-                      расстройство личности<br /></span><span class="severeIssuesBox_span4">Расстройства пищевого поведения:<br /></span><span class="severeIssuesBox_span5"
-                      >Анорексия нервоза<br />Булимия нервоза<br />Переедание<br />Расстройства контроля импульсов:<br />Клептомания<br />Патологическая
-                           склонность к азартным играм<br />Пиромания      (навязчивое поджигательство)<br /></span><span class="severeIssuesBox_span6">Зависимости и субстанцевые расстройства:<br /></span><span class="severeIssuesBox_span7"
-                      >Алкогольная зависимость<br />Зависимость от наркотических веществ<br />Табакокурение</span></span>
-                            </h5>
+            <div class="catalogContentBox">
+                <h2 class="catalogSectionTitle">Каталог проблем</h2>
+            </div>
+            <div class="categoryRow">
+                <div class="categoryCol">
+                    <div class="relationshipCategoryCol">
+                        <h2 class="relationshipTitle">Отношения с людьми</h2>
+                        <div class="relationshipSubCategoryRow">
+                            <h2 class="familyColleaguesGroupTitle">
+                                семья<br />коллеги<br />знакомые<br />служащие силовых структур
+                            </h2>
+                            <h2 class="friendsRelativesGroupTitle">
+                                друзья<br />родственники<br />госслужащие<br />незнакомые люди
+                            </h2>
                         </div>
                     </div>
-                <div className="content_box">
-                    <div className="flexColTests">
-                        <h1 className="pageTitle">Тесты</h1>
-                        <div className="flexColTestsItems">
-                            {fetching && !message ? (
-                                <center>
-                                    <Loader />
-                                </center>
-                            ) : message? (
-                                <div className="Title">{message}</div>
-                            ) : (
-                                <ul>
-                                    {problems.map((problem, i) => (
-                                        <li key={`item_${i}`} className="flexRowPersonalityDisorders">
-                                            <h2 className="Title">{ problem["name"] }</h2>
-                                            <Link to={"/catalog/" + problem["id"]}><button className="Button">Подробнее</button></Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                            {role == 5150 && (
-                                <Link to="/admin">
-                                    <button className="Button">Добавить тест</button>
-                                 </Link>
-                            )}
+                    <div class="nutritionCategoryCol">
+                        <h2 class="nutritionTitle">Питание</h2>
+                        <div class="nutritionSubCategoryRow">
+                            <h2 class="carbWithdrawalVegetarianTitle">отказ от углеводов<br />вегетерианство</h2>
+                            <h2 class="fastingDaysIntervalFastingTitle">разгрузочные дни<br />интервальное голодание</h2>
+                        </div>
+                    </div>
+                    <div class="businessCategoryCol">
+                        <h2 class="businessTitle">Бизнес</h2>
+                        <div class="businessSubCategoryRow">
+                            <h2 class="financialCeilingGoalAchievementTitle">финансовый потолок<br />достижение цели</h2>
+                            <h2 class="resourceStateBankruptcyTitle">ресурсное состояние<br />банкротство</h2>
+                        </div>
+                    </div>
+                    <div class="psychIssuesCategoryCol">
+                        <h2 class="psychIssuesTitle">Психологические проблемы</h2>
+                        <div class="psychIssuesSubCategoryRow">
+                            <h2 class="emotionalBurnoutDepressionTitle">
+                                эмоциональное выгорание<br />депрессия<br />детские психотравмы<br />насилие
+                            </h2>
+                            <h2 class="phobiasNegativeEmotionsBreakupTitle">
+                                фобии<br />негативные эмоции<br />расставание<br />утрата ближнего
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="healthCategoryCol">
+                        <h2 class="healthTitle">Здоровье</h2>
+                        <div class="healthSubCategoryRow">
+                            <h2 class="alcoholismVirusDiseasesTitle">алкоголизм<br />вирусные заболевания</h2>
+                            <h2 class="drugAddictionDiarrheaTitle">наркотическая зависимость<br />диарея</h2>
+                        </div>
+                    </div>
+                    <div class="sportsCategoryCol">
+                        <h2 class="sportsTitle">Спорт</h2>
+                        <div class="sportsSubCategoryRow">
+                            <h2 class="injuryMotivationVictoryTitle">травма<br />мотивация на победу</h2>
+                            <h2 class="bullyingEnvyRivalsTitle">буллинг<br />зависть соперников</h2>
                         </div>
                     </div>
                 </div>
+                <div class="testsContentBox">
+                    <div class="testsCategoryCol">
+                        <h2 class="testsTitle">Тесты</h2>
+                        <div class="relationshipTestRow">
+                        {fetching && !message ? (
+                            <center>
+                                <Loader />
+                            </center>
+                        ) : problems.length < 1 || message? (
+                            <div className="noUser">{message}</div>
+                        ) : (
+                            <ul>
+                                {problems.map((problem, i) => (
+                                    <li key={`item_${i}`}>
+                                        <div className="relationshipTestRow">
+                                            <h2 className="relationshipTestTitle">{ problem["name"] }</h2>
+                                            <Link to={"/catalog/" + problem["id"]}><button className="relationshipTestButton">Подробнее</button></Link>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+
+                    {role == 5150 && (
+                                <Link to="/admin">
+                                    <button className="addTestButton">Добавить тест</button>
+                                 </Link>
+                            )}
+                </div>
+            </div>
             </div>
         </section>
 
@@ -159,6 +188,69 @@ const Catalog = () => {
         new Sticky('.sticky-effect');
     </script>
 </body>
+        
+
+            /* <div className="flexRow">
+                <img className="image1" src={text} alt="alt text" />
+                <div className="contentBox">
+                    </div>
+                <div className="infoBox">
+                    <div className="infoCol">
+                        <h1 className="headerTitle">Тесты</h1>
+                        <div className="infoRows">
+                        {fetching && !message ? (
+                            <center>
+                                <Loader />
+                            </center>
+                        ) : problems.length < 1 || message? (
+                            <div className="noUser">{message}</div>
+                        ) : (
+                            <ul>
+                                {problems.map((problem, i) => (
+                                    <li key={`item_${i}`}>
+                                        <div className="infoColumns">
+                                            <h2 className="impulseControlDisordersTitle">{ problem["name"] }</h2>
+                                            <button className="viewButton">Подробнее</button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="sideCol">
+                <div className="sideContent">
+                    <div className="sideRow">
+                        {/* <img className="sideIcon" src={logo} alt="alt text" />
+                        <div className="sideRows">
+                            <div className="sideRowContent"> */
+                                /* <Link to="/"><h2 className="homeTitle">Главная</h2></Link>
+                                <Link to="/profile"><h2 className="profileTitle">Личный кабинет</h2></Link>
+                                <Link to="/catalog"><h2 className="problemCatalogTitle">Каталог проблем</h2></Link>
+                                
+                            </div>
+                            <div className="accountActions">
+                                <Link to="/login"><h2 className="loginTitle">Войти</h2></Link>
+                                <Link to="/register"><h2 className="registerTitle">Регистрация</h2></Link> 
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="problemCatalogBox">
+                    <h2 className="problemCatalog">Каталог проблем</h2>
+                </div>
+            </div> */
+        /* </section>
+
+    </main>
+    <script type="text/javascript">
+        AOS.init();
+        new Sticky('.sticky-effect');
+    </script>
+</body> */
 
     );
 }
