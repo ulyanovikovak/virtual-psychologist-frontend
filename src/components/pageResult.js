1e9f72bee36c356c5b8a7c38908fb18606d6f467
ulianovikova <yasenevsk.18@gmail.com>
2024-06-06 16:17:04 +0300

    update header

--- src/components/pageResult.js
+++ src/components/pageResult.js
@@ -1,17 +1,92 @@
+
+
 import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from '../api/axios';
-import { useNavigate, Link } from "react-router-dom";
+import axios, { axiosTest } from '../api/axios';
+import { useNavigate, Link, useParams } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import React from 'react';
-import '../result.css';
-import logo from "../assets/logo.png"
 import Loader from "./Loader";
 
-
+import '../result.css';
+import logo from "../assets/logo.png"
 
 const Result = () => {
+    const navigate = useNavigate();
+    const logout = useLogout();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [errMsg, setErrMsg] = useState('');
+    const [fetching, setFetching] = useState(true);
+    
+    const [ name, setName ] = useState("");
+    const [ about, setAbout ] = useState("");
+    const [ timeStart, setTimeStart ] = useState("");
+    const [ timeEnd, setTimeEnd ] = useState("");
+    const [ nodes, setNodes ] = useState([]);
+    
+    const RESULT_URL = '/api/results';
+
+    const signOut = async () => {
+        await logout();
+        setLoggedIn(false);
+        navigate('/');
+    }
+
+    const getResult = async () => {
+        if (localStorage.getItem("access")) {
+            setLoggedIn(true);
+        }
+        axios.get("https://9ww9.ru:80" + RESULT_URL, {
+            headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVnSGZHOUYiLCJ0b2tlbiI6IkpQYkl1cm92dHQvSE5oY2xkN1REd0FWSk82UDRMb3BQQ3F0OHpXWVhzQ2hiOFdLOG9oVVBrUWdHOUUvRDdQWEQiLCJpYXQiOjE3MTU1MzkwMTB9.js6b_XCmv1zAJKmy12ahTM-9iRe6g9wSQw4cN656j74' },
+            withCredentials: true
+        }).then((response) => {
+            console.log(JSON.stringify(response?.data[0]));
+            setName(response?.data[0].testName);
+            setAbout(response?.data[0].testDescription);
+            setTimeStart(response?.data[0].timeStart);
+            setTimeEnd(response?.data[0].timeEnd);
+            setNodes(response?.data[0].nodes);
+            setErrMsg("");
+            setFetching(false);
+        }).catch((err) => {
+            if (err.response) {
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+                setErrMsg('Result display Failed: ' + err?.response)
+            } else if (err.request) {
+                setErrMsg('No Server Response');
+                console.log(err.request);
+            } else {
+                console.log('Error', err.message);
+            }
+            console.log(err.config);
+        });
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("access")) {
+            setLoggedIn(true);
+        }
+        setFetching(true)
+        getResult()
+    }, [])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
     <div class="flex-column">
     <main class="pageresults main">
@@ -19,15 +94,24 @@ const Result = () => {
             <div class="headerContainer">
                 <img class="logoImage" src={logo} alt="alt text" />
                 <div class="navigationContainer">
-                    <div class="primaryNavItems">
-                        <h2 class="primaryNavItem">Главная</h2>
-                        <h2 class="primaryNavItem">Каталог проблем</h2>
-                        <h2 class="primaryNavItem">Личный кабинет</h2>
+                    <div className="primaryNavItems">
+                        <Link to="/"><h2 className="primaryNavItem">Главная</h2></Link>
+                        <Link to="/profile"><h2 className="primaryNavItem">Личный кабинет</h2></Link>
+                        <Link to="/catalog"><h2 className="primaryNavItem">Каталог проблем</h2></Link>
                     </div>
-                    <div class="secondaryNavItems">
-                        <h2 class="secondaryNavItem">Войти</h2>
-                        <h2 class="secondaryNavItem">Регистрация</h2>
+                    <div className="secondaryNavItems">
+                    {loggedIn ? (
+                        <button onClick={signOut}><h5 className="secondaryNavItem">Выйти</h5></button>
+                    ) : (
+                        <>
+                            <Link to="/login"><h2 className="secondaryNavItem">Войти</h2></Link>
+                            <Link to="/register"><h2 className="secondaryNavItem">Регистрация</h2></Link>
+                        </>
+                    )}
                     </div>
+
+
+
                 </div>
             </div>
             <div class="mainContent">
