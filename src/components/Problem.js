820eebf1640fa2eb9b22526d43a8d4c3529bf578
yukiyuyyu <y.d.morozova@yandex.ru>
2024-06-06 21:30:05 +0300

    problems

--- src/components/Problem.js
+++ src/components/Problem.js
@@ -4,7 +4,7 @@ import { useNavigate, Link, useParams } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import React from 'react';
 
-import '../profile.css';
+import '../problem.css'
 import logo from "../assets/logo.png"
 
 const Problem = () => {
@@ -90,67 +90,54 @@ const Problem = () => {
     }, [])
 
     return (
-        <div className="flex-column">
-            <main className="profile main">
-                <section className="personalDataSection">
-                    <div className="contentBox">
-                        <h2 className="mediumTitle">{name}</h2>
-                    </div>
+        <body className="flex-column">
+    <main className="problem-page main">
+        <section className="problemDescriptionSection">
+            <div className="flexCol">
+                <div className="contentBox1">
                     <div className="flexRow">
-                        <div className="flexCol">
-                            <div className="contentBox1">
-                                <div className="flexCol1">
-                                    <div className="flexCol2">
-                                        <p id="uidnote" className="instructions">
-                                            {about}
-                                        </p>
-                                    </div>
-                                </div>
+                        <img className="image" src={logo} alt="alt text" />
+                        <div className="flexRow1">
+                            <div className="flexRow2">
+                                <Link to="/"><h2 className="mainTitle">Главная</h2></Link>
+                                <Link to="/profile"><h2 className="personalCabinetTitle">Личный кабинет</h2></Link>
+                                <Link to="/catalog"><h2 className="catalogProblemsTitle">Каталог проблем</h2></Link>
                             </div>
-                            {showLinks && (
-                                <div className="contentBox1">
-                                    <div className="flexRow1">
-                                        <button className="changeDataTitle">
-                                            <a href={test}>Пройти тест</a>
-                                        </button>
-                                        <button className="changeDataTitle">
-                                            <a href={form}>Пройти форму</a>
-                                        </button>
-                                    </div>
-                                </div>
-                            )}
-                            <div className="contentBox1">
-                                <div className="flexRow1">
-                                    <button className="changeDataTitle" onClick={handleBuy}>
-                                        Купить
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="navigationContentBox">
-                        <div className="flexRow3">
-                            <img className="image" src={logo} alt="alt text" />
-                            <div className="flexRow4">
-                                <Link to="/"><h2 className="navigationTitle">Главная</h2></Link>
-                                <Link to="/profile"><h2 className="navigationTitle1">Личный кабинет</h2></Link>
-                                <Link to="/catalog"><h2 className="navigationTitle">Каталог проблем</h2></Link>
-                            </div>
-                            <div className="authContainer">
-                                {loggedIn ? (
-                                    <button onClick={signOut}><h5 className="loginLink">Выйти</h5></button>
+                            {loggedIn ? (
+                                    <button onClick={signOut}><h5 className="logoutTitle">Выйти</h5></button>
                                 ) : (
                                     <>
-                                        <Link to="/login"><h2 className="loginLink">Войти</h2></Link>
-                                        <Link to="/register"><h2 className="registerLink">Регистрация</h2></Link>
+                                        <Link to="/login"><h2 className="logoutTitle">Войти</h2></Link>
+                                        <Link to="/register"><h2 className="logoutTitle">Регистрация</h2></Link>
                                     </>
                                 )}
-                            </div>
                         </div>
                     </div>
-                </section>
-            </main>
-        </div>
+                </div>
+                <div className="contentBox">
+                    <h1 className="problemDescriptionTitle">{name}</h1>
+                </div>
+            </div>
+            <div className="flexCol1">
+                <h1 className="bullshitParagraph">
+                   {about}
+                </h1>
+                <div className="flexRow3">
+                    <button className="buyTestButton">Купить тест</button>
+                    <div className="flexCol2">
+                        <h1 className="linkTitle1">{test}</h1>
+                        <h1 className="linkTitle2">{form}</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <script type="text/javascript">
+        AOS.init();
+        new Sticky('.sticky-effect');
+    </script>
+</body>
     );
 };
 
